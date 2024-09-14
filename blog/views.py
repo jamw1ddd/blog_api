9a1cfb84eabf9f937891282cf8cf99f04dcfc0f8
Jamshid <jamw1ddd2002@gmail.com>
2024-09-14 16:47:04 +0500

    'mixins

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,15 @@
-from rest_framework import viewsets
+from rest_framework import viewsets,mixins 
 
 from blog.models import Category,Post
 from blog.serializers import CategorySerializer,PostSerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(viewsets.mixins.CreateModelMixin,
+                   mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   mixins.ListModelMixin,
+                   viewsets.GenericViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
 
